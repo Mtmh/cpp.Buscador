e1c9d6359235c0aaab0ed0adb6722ff5881a01e8
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-06-10 12:32:18 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string lee(ifstream &a); // a== archivo
+int cuenta(const string &contenido, char c); // c==contenido
+
+
+int main()
+{
+    int cantidad;
+    string contenido;
+    ifstream españa;
+    españa.open ("/Users/tizianomartinhernando/Desktop/raiz/c++/DuarteCurso/Buscador/España.txt");
+
+    if (!españa.is_open ()){
+        cout << "Error: No se ha podido abrir el archivo" << endl;
+        return 1;
+    }
+
+    contenido = lee(españa);
+    cantidad = cuenta(contenido, '.'); // lo q quieras Buscar A..33
+    cout << cantidad << endl;
+
+    return 0;
+}
+
+string lee(ifstream &a)
+{
+    string s, contenido;
+
+    while (getline (a, s))
+        contenido += s + "\n";
+    return contenido;
+}
+
+int cuenta(const string &contenido, char c)
+{
+    int contador = 0;
+
+    string::const_iterator i;
+
+    for (i = contenido.begin (); i != contenido.end (); ++i)
+        if (*i == c)
+            ++contador;
+    return contador;
+
+}
